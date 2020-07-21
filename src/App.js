5e7e1a8b11586cfe280b83e7421eb92771fb0f96
Ahmadjon <info.ahmadjon@gmail.com>
2020-07-21 19:01:37 +0500

    ready api project that shows GitHub jobs list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FetchJobs from './Fetch-jobs';
 import {Container} from 'react-bootstrap'
-
+import Job from './job'
+import Pagination from './JobPagination'
+import SearchForm from './searchForm'
 function App(){
-  const {loading, jobs, error} = FetchJobs()
 
+  const [params, setParams ] = useState({})
+  const [page, setPage] = useState(1)
+
+  const {loading, jobs, error} = FetchJobs(params, page)
+
+  function handleParamChange(e) {
+    const param = e.target.name
+    const val = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return {...prevParams, [param] : val}
+    })
+
+  }
 
   return (
-    <Container>
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <Pagination page={page} setPage={setPage} hasNextPage={true} />
       {loading && <h1>Loading...</h1>}
       {error && <h2>Error....</h2>}
-      <h1>{jobs.length}</h1>
+      
+      {
+        jobs.map(job => {
+          return <Job key={job.id} job={job} />
+        })
+      }
+      <Pagination page={page} setPage={setPage} hasNextPage={true} />
     </Container>
     )
 }
